9384ac75d8bed2c5cd969127ebd6532f0c7e9af4
Guilherme Monteiro <guilherme.montnezes@gmail.com>
2022-04-04 10:30:53 -0300

    Initial Project

---
+++ src/test/java/com/grupo01/financeapi/FinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo01.financeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
